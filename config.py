11945eb244e6777489138daffe07a67f0379723c
Andres Ventura <elelson06@users.noreply.github.com>
2026-04-08 21:05:26 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+import os
 
+BOOKITIT_PUBLIC_KEY  = os.environ["BOOKITIT_PUBLIC_KEY"]
+WIDGET_URL           = os.environ["WIDGET_URL"]
+CHECK_INTERVAL_MIN   = int(os.environ.get("CHECK_INTERVAL_MIN", "5"))
+TELEGRAM_TOKEN       = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_CHAT_ID     = os.environ["TELEGRAM_CHAT_ID"]
